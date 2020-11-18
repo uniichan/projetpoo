93dd91ebe76fdd81c73a7e987b94d4744ab7797e
Wassim BEN AISSA <wassim.benaissa@viacesi.fr>
2020-11-18 15:25:11 +0100

    modif

---
+++ test/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,24 @@
+
+
+#include <iostream>
+#include "main.h"
+using namespace std;
+
+int main()
+{
+    double code;
+    std::cout << "Numéro d'utilisateur :\n";
+    std::cin >> code;
+    if (code = 007)
+    {
+        std::cout << "Accès autorisé";
+    }
+    else
+    {
+        std::cout << "Accès refusé";
+    }
+
+
+}
+
+
